df13a359152d62e92f8e85aa174847ec1a974ddd
nirina78u <nirina786zweb@gmail.com>
2026-01-02 14:53:32 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+
+function toggle_login() {
+  const content_login =document.getElementById('content_login') ;
+
+  if (content_login.style.display === "none") {
+    content_login.style.display = "block" ;
+  }
+
+  else {
+    content_login.style.display = "none" ;
+  }
+}
+
+function toggle_menue() {
+   const mobile_head =document.getElementById('mobile_head') ;
+
+     if (mobile_head.style.display === "none") {
+    mobile_head.style.display = "block" ;
+  }
+
+  else {
+    mobile_head.style.display = "none" ;
+  }
+
+}
